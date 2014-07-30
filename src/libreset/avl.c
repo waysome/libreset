0c5fd70a863110bacbea8df954e14a530a2bcecf
Matthias Beyer <mail@beyermatthias.de>
2014-07-30 15:27:56 +0200

    Added helper function for finding elements

--- src/libreset/avl.c
+++ src/libreset/avl.c
@@ -2,6 +2,25 @@
 
 #include <stdlib.h>
 
+/**
+ * Find an element and its parent
+ *
+ * Helper function to find an element and its parent, for simplification of
+ * avl_find() and avl_del() functions.
+ *
+ * @return 1 if the element is found, else 0
+ */
+static int
+find_element_with_parent(
+    struct avl_el* root, //!< root of the tree
+    int (*cmp)(void* const, void* const, void*), //!< comparator function
+    void* etc, //!< additional argument for the comperator function
+    struct avl_el** parent_dest, //!< destination of the ptr to the parent
+    struct avl_el** found_dest //!< destination of the ptr to the element
+) {
+    return 0;
+}
+
 struct avl_el*
 avl_alloc(void) {
     return avl_add(NULL, NULL, NULL);
