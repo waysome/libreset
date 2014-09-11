4f8ff6cf9314a42228b974030e7846498cd2d534
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-09-11 20:58:16 +0200

    Restructure avl.c

--- src/libreset/avl.c
+++ src/libreset/avl.c
@@ -1,11 +1,54 @@
 #include <stdlib.h>
 
-#include "libreset/avl.h"
+#include "avl.h"
+
+
+/*
+ *
+ *
+ * forward declarations
+ *
+ *
+ */
+
+static void
+destroy_subtree(
+    struct avl_el* node //!< A node to destroy
+);
+
+
+/*
+ *
+ *
+ * interface implementation
+ *
+ *
+ */
 
 struct avl*
 avl_alloc(void) {
     return calloc(1, sizeof(struct avl));
 }
+
+int
+avl_destroy(
+    struct avl* avl //!< The avl tree
+) {
+    if (avl && avl->root) {
+        destroy_subtree(avl->root);
+    }
+    free(avl);
+    return 1;
+}
+
+
+/*
+ *
+ *
+ * implementation of internal functions
+ *
+ *
+ */
 
 static void
 destroy_subtree(
@@ -21,13 +64,3 @@ destroy_subtree(
     free(node);
 }
 
-int
-avl_destroy(
-    struct avl* avl //!< The avl tree
-) {
-    if (avl && avl->root) {
-        destroy_subtree(avl->root);
-    }
-    free(avl);
-    return 1;
-}
