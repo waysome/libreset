bc3b0b6127904d0ce85d6883dfbeb6336f0a8dbe
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-09-14 17:57:29 +0200

    Add const attribute to real functions

--- src/libreset/bloom.h
+++ src/libreset/bloom.h
@@ -36,6 +36,8 @@
 
 #include "libreset/hash.h"
 
+#include "util/attributes.h"
+
 /**
  * Bloom filter type
  *
@@ -49,7 +51,7 @@ typedef size_t bloom;
  * @memberof bloom
  * @return bloom filter for the given hash
  */
-bloom
+__const__ bloom
 bloom_from_hash(
     rs_hash hash //!< hash value for which to calculate the bloom filter
 );
@@ -62,7 +64,7 @@ bloom_from_hash(
  * @memberof bloom
  * @return 0 if the element is not in the set, 1 otherwise
  */
-int
+__const__ int
 bloom_may_contain(
     bloom element, //!< element, represented as a bloom filter
     bloom set //!< bloom filter representing the set to check
@@ -74,7 +76,7 @@ bloom_may_contain(
  * @memberof bloom
  * @return 0 if the sets represented are exclusively disjunctive, 1 otherwise
  */
-int
+__const__ int
 bloom_may_have_common(
     bloom a, //!< first of the bloom filters to check for common elements
     bloom b //!< second of the bloom filters to check for common elements
