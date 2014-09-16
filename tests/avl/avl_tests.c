48c2fcfbefccb1ed3a3740e97982a54da7e8b131
Matthias Beyer <mail@beyermatthias.de>
2014-09-16 12:57:22 +0200

    Added test for testing allocation and deleting for AVL

--- tests/avl/avl_tests.c
+++ tests/avl/avl_tests.c
@@ -14,6 +14,16 @@ START_TEST (test_avl_alloc) {
 }
 END_TEST
 
+START_TEST (test_avl_alloc_destroy) {
+    struct avl* avl = avl_alloc();
+
+    ck_assert(avl != NULL);
+    ck_assert(avl->root == NULL);
+
+    ck_assert_int_eq(1, avl_destroy(avl, NULL));
+}
+END_TEST
+
 Suite*
 suite_avl_create(void) {
     Suite* s;
@@ -26,6 +36,7 @@ suite_avl_create(void) {
 
     /* test adding to test cases */
     tcase_add_test(case_allocfree, test_avl_alloc);
+    tcase_add_test(case_allocfree, test_avl_alloc_destroy);
 
     /* Adding test cases to suite */
     suite_add_tcase(s, case_allocfree);
