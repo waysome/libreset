4d6f56b1d5618a86ee0c4650f2014dbb52ef8d6e
Matthias Beyer <mail@beyermatthias.de>
2014-09-16 12:56:23 +0200

    Added allocation test for avl test

--- tests/avl/avl_tests.c
+++ tests/avl/avl_tests.c
@@ -1,19 +1,34 @@
 #include <check.h>
 
+#include <stdlib.h>
+
 #include "avl.h"
+
+START_TEST (test_avl_alloc) {
+    struct avl* avl = avl_alloc();
+
+    ck_assert(avl != NULL);
+    ck_assert(avl->root == NULL);
+
+    free(avl);
+}
+END_TEST
 
 Suite*
 suite_avl_create(void) {
     Suite* s;
-    TCase* case_insert;
+    TCase* case_allocfree;
 
     s = suite_create("AVL");
 
     /* Test case creation */
-    case_insert = tcase_create("");
+    case_allocfree = tcase_create("Allocating and deleting");
+
+    /* test adding to test cases */
+    tcase_add_test(case_allocfree, test_avl_alloc);
 
     /* Adding test cases to suite */
-    suite_add_tcase(s, case_insert);
+    suite_add_tcase(s, case_allocfree);
 
     return s;
 }
