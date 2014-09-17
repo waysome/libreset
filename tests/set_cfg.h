18f068ea882771685943d1725d79a81c0be30fb8
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-09-17 12:58:20 +0200

    Add set configuration

---
+++ tests/set_cfg.h
@@ -0,0 +1,21 @@
+/**
+ * @file set_cfg.h
+ *
+ * This is the interface definition for the AVL tree implementation. The
+ * implementation of the AVL element type is public, but the provided functions
+ * should be used when interacting with it.
+ *
+ * @authors Julian Ganz
+ *
+ * @copyright See the LICENSE file shipped with the repository.
+ */
+
+#ifndef __SET_CFG__
+#define __SET_CFG__
+
+#include "libreset/set.h"
+
+extern struct r_set_cfg cfg_int;
+
+#endif // __SET_CFG__
+
