73def27c2e5d87984d4dc32dbeaf7fe21bd910b3
Matthias Beyer <mail@beyermatthias.de>
2014-09-18 21:02:22 +0200

    Added debug print helper for avl.c

--- src/libreset/avl.c
+++ src/libreset/avl.c
@@ -4,11 +4,19 @@
 
 #include "util/likely.h"
 #include "util/macros.h"
+#include "util/debug.h"
 
 #include "ll.h"
 #include "avl.h"
 #include "util/macros.h"
 
+/**
+ * Debug print helper for avl implementation code
+ *
+ * @note No #ifdef DEBUG here, because if dbg() evaluates to nothing, this code
+ * gets removed by the compiler anyways.
+ */
+#define avl_dbg(fmt,...) do { dbg("avl: "fmt, __VA_ARGS__); } while (0)
 
 /*
  *
