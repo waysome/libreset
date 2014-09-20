4ac29d983fcd02542998d845dc5cc9e5008e5f4d
Matthias Beyer <mail@beyermatthias.de>
2014-09-20 17:58:30 +0200

    Added set.c file and r_set type implementation

---
+++ src/libreset/set.c
@@ -0,0 +1,30 @@
+/*
+ * libreset - Reentrent set library for fast set operations in C
+ *
+ * Copyright (C) 2014 Matthias Beyer
+ * Copyright (C) 2014 Julian Ganz
+ *
+ * This file is part of libreset.
+ *
+ * libreset is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * libreset is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libreset. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "libreset/set.h"
+
+#include "ht.h"
+
+struct r_set {
+    struct ht ht;
+    struct r_set_cfg* cfg;
+};
