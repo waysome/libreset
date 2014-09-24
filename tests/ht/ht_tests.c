529262056d1bde949719d3ff7ec848684349c520
Matthias Beyer <mail@beyermatthias.de>
2014-09-24 20:07:26 +0200

    Added test for allocating more than just 2 buckets

--- tests/ht/ht_tests.c
+++ tests/ht/ht_tests.c
@@ -16,6 +16,21 @@ START_TEST (test_ht_init) {
 }
 END_TEST
 
+START_TEST (test_ht_init_big) {
+    size_t exp;
+
+    for (exp = 2; exp < 10; exp++) {
+        struct ht ht;
+        ht_init(&ht, exp); /* allocate 2^exp */
+
+        ck_assert(ht.buckets != NULL);
+        ck_assert(ht_nbuckets(&ht) == (1 << exp)); /* (1 << exp) == 2^exp */
+
+        ht_destroy(&ht, &cfg_int);
+    }
+}
+END_TEST
+
 Suite*
 suite_ht_create(void) {
     Suite* s;
@@ -32,6 +47,7 @@ suite_ht_create(void) {
 
     /* test adding to test cases */
     tcase_add_test(case_allocfree, test_ht_init);
+    tcase_add_test(case_allocfree, test_ht_init_big);
 
     /* Adding test cases to suite */
     suite_add_tcase(s, case_allocfree);
