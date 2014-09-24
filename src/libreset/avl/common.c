7fffb04292fcdc4208f7d208b9d4dd36841357c2
Matthias Beyer <mail@beyermatthias.de>
2014-09-24 13:15:53 +0200

    Fix: run astyle on avl/common.c

--- src/libreset/avl/common.c
+++ src/libreset/avl/common.c
@@ -59,7 +59,7 @@ rebalance_subtree(
 
     // check whether the subtrees is already balanced (see paper)
     if (avl_node_cnt(root) >
-        (unsigned int) (1 << (avl_height(root) - 1) ) - 1) {
+            (unsigned int) (1 << (avl_height(root) - 1) ) - 1) {
         avl_dbg("Subtree already balanced for %p", root);
         return root;
     }
