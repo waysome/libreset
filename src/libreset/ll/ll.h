84216c9e5fa0d604f75e057bf45e825989fdd323
Matthias Beyer <mail@beyermatthias.de>
2014-09-25 11:16:56 +0200

    Fixup: ll_select() param documentation

--- src/libreset/ll/ll.h
+++ src/libreset/ll/ll.h
@@ -202,7 +202,7 @@ ll_select(
     struct ll const* src, //!< The source from where to select
     r_predf pred, //!< The predicate
     void* etc, //!< Additional information for the predicate function
-    int (*insf)(void* elem, void* ins_etc), //!< Function to insert somewhere
+    int (*insf)(void* elem, void* ins_etc), //!< insert function
     void* ins_etc //!< additional parameter for `insertf`
 );
 
