c8d9e95e1698c652f708152582d2a56af739c5d8
Matthias Beyer <mail@beyermatthias.de>
2014-09-25 02:37:38 +0200

    Add: Implementation for ht_select()

--- src/libreset/ht/ht_select.c
+++ src/libreset/ht/ht_select.c
@@ -8,4 +8,16 @@ ht_select(
     void* etc,
     struct r_set_cfg* const cfg
 ) {
+    size_t i;
+    size_t nbucks = ht_nbuckets(src);
+
+    struct insert_etc_data etc_data = { .ht = dest, .cfg = cfg };
+
+    for (i = 0; i < nbucks; i++) {
+        if (!avl_select(&src->buckets[i].avl, pred, etc, insert, &etc_data)) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
