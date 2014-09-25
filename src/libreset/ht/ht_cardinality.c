cb70ca2237f38fd1d18141b84c1d1168079f3eb3
Marcel Mueller <com2040@gmail.com>
2014-09-25 11:22:57 +0200

    Added ht_cardinality implementation

---
+++ src/libreset/ht/ht_cardinality.c
@@ -0,0 +1,15 @@
+#include "ht.h"
+
+int
+ht_cardinality(
+    struct ht const* ht
+) {
+    size_t i = ht_nbuckets(ht);
+    int card = 0;
+
+    while(--i) {
+        card += avl_cardinality(&ht->buckets[i].avl);
+    }
+
+    return card;
+}
