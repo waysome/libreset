25975a7d3a099375b328b78b74ae8a99c3cdd0c3
Matthias Beyer <mail@beyermatthias.de>
2014-09-25 02:23:33 +0200

    Added implementation for avl_select() including helper func

--- src/libreset/avl/avl_select.c
+++ src/libreset/avl/avl_select.c
@@ -1,5 +1,34 @@
 #include "avl/avl.h"
 
+static int
+select_from_subtree(
+    struct avl_el* root,
+    r_predf pred,
+    void* etc,
+    int (*insf)(void* elem, void* insetc),
+    void* insetc
+) {
+    if (!root) {
+        return 0;
+    }
+
+    int l = 0;
+    int r = 0;
+    int base = 0;
+
+    if (root->l) {
+        l = select_from_subtree(root->l, pred, etc, insf, insetc);
+    }
+
+    if (root->r) {
+        r = select_from_subtree(root->r, pred, etc, insf, insetc);
+    }
+
+    base = ll_select(&root->ll, pred, etc, insf, insetc);
+
+    return (base | l | r);
+}
+
 int
 avl_select(
     struct avl const* src,
@@ -8,5 +37,10 @@ avl_select(
     int (*insf)(void* elem, void* insetc),
     void* insetc
 ) {
+    if (src) {
+        return select_from_subtree(src->root, pred, etc, insf, insetc);
+    }
+
+    return 1;
 }
 
