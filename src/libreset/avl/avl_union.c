fca583281484407870da9466e66d26f416478059
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-09-27 01:06:28 +0200

    Check for the existence of a node before inserting a new one

--- src/libreset/avl/avl_union.c
+++ src/libreset/avl/avl_union.c
@@ -23,6 +23,7 @@ static int
 merge_trees(
     struct avl_el** dest, //!< destination subtree
     struct avl_el const* src, //!< source subtree
+    struct avl* dest_all, //!< the entire destination tree
     struct r_set_cfg const* cfg //!< type information provided by the user
 )
 __r_nonnull__(1, 2)
@@ -42,7 +43,7 @@ avl_union(
     struct avl const* src,
     struct r_set_cfg const* cfg
 ) {
-    int retval = merge_trees(&dest->root, src->root, cfg);
+    int retval = merge_trees(&dest->root, src->root, dest, cfg);
     dest->root = rebalance_subtree(dest->root);
     return retval;
 }
@@ -59,6 +60,7 @@ int
 merge_trees(
     struct avl_el** dest,
     struct avl_el const* src,
+    struct avl* dest_all,
     struct r_set_cfg const* cfg
 ) {
     // if the source is empty, all it's items are integrated into dest
@@ -70,6 +72,13 @@ merge_trees(
 
     // if the destination subtree is empty, we copy over the subtree
     if (!*dest) {
+        // check whether the node we want to merge is in the tree already
+        if (find_node(dest_all, src->hash)) {
+            // we already have a node, we just have to find it...
+            return merge_trees(&dest_all->root, src, dest_all, cfg);
+        }
+
+        // nope, it isn't
         *dest = new_avl_el(src->hash);
         if (!*dest) {
             retval = -ENOMEM;
@@ -81,26 +90,26 @@ merge_trees(
     // if there is a node for dest->hash in the src, it's in the left subtree
     else if (src->hash > (*dest)->hash) {
         // the current src node has to be merged in the right subtree
-        retval = merge_trees(&(*dest)->r, src, cfg);
+        retval = merge_trees(&(*dest)->r, src, dest_all, cfg);
         if (retval < 0) {
             goto cleanup;
         }
 
         // search in the left source subtree
-        retval = merge_trees(dest, src->l, cfg);
+        retval = merge_trees(dest, src->l, dest_all, cfg);
 
         goto cleanup;
     }
     // if there is a node for dest->hash in the src, it's in the right subtree
     else if (src->hash < (*dest)->hash) {
         // the current src node has to be merged in the left subtree
-        retval = merge_trees(&(*dest)->l, src, cfg);
+        retval = merge_trees(&(*dest)->l, src, dest_all, cfg);
         if (retval < 0) {
             goto cleanup;
         }
 
         // search in the right source subtree
-        retval = merge_trees(dest, src->r, cfg);
+        retval = merge_trees(dest, src->r, dest_all, cfg);
 
         goto cleanup;
     }
@@ -108,13 +117,13 @@ merge_trees(
     // at this point src->hash == (*dest) -> hash
 
     // copy/recurse into the left subtrees
-    retval = merge_trees(&(*dest)->l, src->l, cfg);
+    retval = merge_trees(&(*dest)->l, src->l, dest_all, cfg);
     if (retval < 0) {
         goto cleanup;
     }
 
     // copy/recurse into the right subtrees
-    retval = merge_trees(&(*dest)->r, src->r, cfg);
+    retval = merge_trees(&(*dest)->r, src->r, dest_all, cfg);
     if (retval < 0) {
         goto cleanup;
     }
